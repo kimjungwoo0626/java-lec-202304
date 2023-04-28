b1ed72813895b243cef24a2b172029810196228d
봇치 <nmbbstone1@naver.com>
2023-04-28 21:57:29 +0900

    2일차에 배운 것

---
+++ Day02/src/HelloWorld02.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld02 {
+
+	 public static void main(String args[]) {
+		 System.out.println("오늘은 2일차야");
+		 	 }
+}
---
+++ Day02/src/JavaStructure.java
@@ -0,0 +1,14 @@
+/*컴파일 에러를 발생시키는 기초 문법
+1. 자바는 클래스이름과 파일명이 같아야한다.
+2. 자바는 대소문자를 구분합니다.
+ ex) Java java -> 틀리다.
+3. {} 중괄호로 구분한다.
+4. 자바의 끝은 ;을 사용한다. */
+
+public class JavaStructure 
+{
+	public static void main(String args[]) {
+		System.out.println("후우후우");
+		System.out.println("후우후우");
+	}
+}
