72c1be9a56d890fd6a6a765103a8d8243cf52e8e
봇치 <nmbbstone1@naver.com>
2023-05-02 20:25:28 +0900

    Day03

---
+++ Day03/src/VariableExample.java
@@ -0,0 +1,31 @@
+
+public class VariableExample {
+
+	
+	public static void main(String args[]) {
+	
+        //자료형  변수명
+		int a; //<- 변수 선언
+		a = 40;
+	
+		
+		int b = 20; //<- 변수 선언과 동시에 초기화
+		//int age; // 같은 이름의 변수는 선언 불가(컴파일에러)
+		
+		
+		// 변수를 사용한 부분
+		int Sum = a + b;
+		System.out.println("나이 + 나이2 = " + Sum);
+		
+		/*
+		 * 자바 예약어는 변수명으로 쓸 수 없다.
+		 int if;
+		 int for;
+		 int break;
+		 */
+		
+// ex) int 김치박스
+// 김치박스 = 10;
+	}
+
+}
