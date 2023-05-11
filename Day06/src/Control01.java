c5feb72b66d099d9d14cc87067aa3af5071d9782
봇치 <nmbbstone1@naver.com>
2023-05-11 21:06:01 +0900

    1

---
+++ Day06/src/Control01.java
@@ -0,0 +1,30 @@
+/*
+ * 1. if
+ * 2. if else if
+ * 3. if else
+ */
+
+
+
+public class Control01 {
+
+	public static void main(String[] args) {
+		
+		int age = 40;
+		
+		if(age <= 39) {
+			if(19 <= age) {
+			System.out.println
+			("19세 이상 39세 이하인 사람은 청년저축 가입 가능합니다.");
+			}
+			}
+		
+		else {
+				System.out.println
+				("39세 이상인 사람은 청년 저축 가입 불가능합니다. ");
+			}
+		
+		System.out.println("프로그램 종료.");
+		
+	}
+}
---
+++ Day06/src/SwitchExample.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("1~7 범위내의 숫자를 넣어주세요 : ");
+		int dayCode = scan.nextInt();
+		
+		switch(dayCode) {
+		case 1 :
+			System.out.println("월요일");
+			break;
+		case 2 :
+			System.out.println("화요일");
+			break;
+		case 3 :
+			System.out.println("수요일");
+			break;
+		case 4 :
+			System.out.println("목요일");
+			break;
+		case 5 :
+			System.out.println("금요일");
+			break;
+		case 6 :
+			System.out.println("토요일");
+			break;
+		case 7 :
+			System.out.println("일요일");
+			break;
+		default :
+			System.out.println("범위는 1 ~7 입니다.");
+		
+		}
+		
+		if( dayCode == 1) {
+			System.out.println("월요일");
+		} else if (dayCode == 2 ) {
+			System.out.println("화요일");
+		} else if (dayCode == 3 ) {
+			System.out.println("수요일");
+		} else if (dayCode == 4 ) {
+			System.out.println("목요일");
+		} else if (dayCode == 5 ) {
+			System.out.println("금요일");
+		} else if (dayCode == 6 ) {
+			System.out.println("토요일");
+		} else if (dayCode == 7 ) {
+			System.out.println("일요일");
+		} else {
+		System.out.println("종료");
+		}
+	}
+	
+}
