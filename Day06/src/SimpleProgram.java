a948d7000e849dff33ab50180a2e8d23fdaebe20
봇치 <nmbbstone1@naver.com>
2023-05-11 21:55:13 +0900

    3

---
+++ Day06/src/SimpleProgram.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		int command = 0;
+		
+		do {
+			
+			System.out.println("명령어를 입력해 주세요. (종료 5) : ");
+			Scanner scan = new Scanner(System.in);
+			command = scan.nextInt();
+			System.out.println("입력된 명령어는 " + command + "입니다. ");
+			
+			
+		} while (command != 5);
+		
+		System.out.println("프로그램 종료");
+		
+	}
+}
